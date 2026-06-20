69a792623028adfefa64a7f4004fa4fcb00c2268
Jashanpreet Kaur <jshnprit.14@gmail.com>
2026-06-20 22:25:44 +0530

    finall

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -87,7 +87,7 @@ const Upload = () => {
               <img src="/images/resume-scan.gif" className="w-full" />
             </>
           ) : (
-            <h2>Drop ypur resume for an ATS score and improvement tips</h2>
+            <h2>Drop your resume for an ATS score and improvement tips</h2>
           )}
           {!isProcessing && (
             <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
